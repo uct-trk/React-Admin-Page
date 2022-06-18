912f2295412093166d70665df44d00a8f26034e4
unknown <ugurcan.uut82@gmail.com>
2022-06-18 17:59:58 +0300

    Customers,Employees,Orders tables

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Edit,
+  Toolbar,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
 
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 const Employees = () => {
   return (
-    <div>Employees</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees"/>
+      <GridComponent id="gridcomp" dataSource={employeesData} allowSorting allowPaging toolbar={['Search']} width="auto">
+        <ColumnsDirective>
+          {employeesGrid.map((item,index) => (
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[ Page, Search,Toolbar]}/>
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Employees
+export default Employees;
